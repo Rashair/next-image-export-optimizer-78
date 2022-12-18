fae8f6777d84e4b7db2be7267546ab34915dd2eb
Rashair <przemyslaw.wk@gmail.com>
2022-12-18 13:13:10 +0100

    Change distDir to reproduce the error

--- example/next.config.js
+++ example/next.config.js
@@ -1,15 +1,18 @@
 module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  distDir: ".build", // That's the issue!
   images: {
     loader: "custom",
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    deviceSizes: [640, 750, 828, 900, 1080, 1440, 1600, 1920, 2048, 3840]
   },
   env: {
     nextImageExportOptimizer_imageFolderPath: "public/images",
-    nextImageExportOptimizer_exportFolderPath: "out",
+    nextImageExportOptimizer_exportFolderPath: ".export",
     nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
     nextImageExportOptimizer_quality: 75,
     nextImageExportOptimizer_storePicturesInWEBP: true,
     nextImageExportOptimizer_generateAndUseBlurImages: true,
-  },
+  }
 };
